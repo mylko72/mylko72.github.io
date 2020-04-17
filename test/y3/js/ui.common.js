eaea2d54e2ee68cff926f03544ac215c119d9e06
mylko <mypmk@naver.com>
2020-04-17 11:13:52 +0900

    y3 프로젝트 추가

---
+++ test/y3/js/ui.common.js
@@ -0,0 +1,211 @@
+(function (win, $, undefined) {
+    "use strict";
+    
+    var SHILLADFS = SHILLADFS || (SHILLADFS = {});      // NAMESPACE 지정
+    SHILLADFS.MD = SHILLADFS.MD || {};                  // 공통 모듈
+    SHILLADFS.UI = SHILLADFS.UI || {};                  // UI 기본 설정
+
+    // UI 기본 설정
+    SHILLADFS.UI = {
+        Components : function(){},
+        LIBS : {},
+        keys : {},
+        elem : {
+            $doc : $(document),
+            $win : $(window),
+            $html : $('html'),
+            $body : $('body'),
+            head : document.getElementsByTagName("head")[0]
+        },
+        events : {
+            resize : "resize",
+            ready : "ready",
+            load : "load",
+            click : "click",
+            mousewheel : "DOMMouseScroll mousewheel wheel",
+            mousemove : "mousemove",
+            mousedown : "mousedown",
+            mouseup : "mouseup",
+            touchstart : "touchstart",
+            touchmove : "touchmove",
+            touchend : "touchend"
+        },
+        keycode : {
+            tab 		: 9,
+            enter 		: 13,
+            up 			: 38,
+            down 		: 40,
+            left 		: 37,
+            right 		: 39,
+            esc 		: 27,
+            backspace 	: 8,
+            space	 	: 32
+        },
+
+        log : function( msg, color ){
+  	        if( msg ){
+		        color || (color = 'red');
+		        window.console.log("%c" + msg, "color:"+color+";");
+	        }
+        },
+
+        debug : function( msg ){
+	        if( msg ){
+		        var $html = '<div id="debug" style="position:fixed;top:0;right:0;z-index:9999;background-color:#000;color:#fff;font-size:14px;"></div>';
+		        if( !$('#debug').length ){
+			        $('body').append( $html );
+		        }
+		        $('#debug').text( msg );
+	        }
+        },
+
+	    transitionEndName : (function(){
+		    var keys,
+			    el = document.createElement('fakeelement'),
+			    transitions = {
+				    'transition':'transitionend',
+				    'OTransition':'oTransitionEnd',
+				    'MozTransition':'transitionend',
+				    'WebkitTransition':'webkitTransitionEnd'
+			    };
+
+		    for( keys in transitions ){
+			    if( el.style[keys] !== undefined ){
+				    return transitions[keys];
+			    }
+		    }
+	    }()),
+
+
+	    animationEndName : (function(){
+		    var keys,
+			    el = document.createElement('fakeelement'),
+			    animations = {
+				    'animation':'animationend',
+				    'OAnimation':'oAnimationEnd',
+				    'MozAnimation':'animationend',
+				    'WebkitAnimation':'webkitAnimationEnd'
+			    };
+
+		    for( keys in animations ){
+			    if( el.style[keys] !== undefined ){
+				    return animations[keys];
+			    }
+		    }
+	    }()),
+
+    };
+
+    // 공통 모듈 정의
+    /* POPUP 
+    * 팝업 닫기 클릭 시 열려있는 모든 팝업 같이 닫힘
+    * 호출 버튼 : [data-ui=popup] && href="{{ String }}"
+    * 호출 팝업 : [data-popup=cont] && id="{{ String }}"  
+    * 팝업 닫기 버튼 : [data-popup=close]
+    * 팝업 dimmed : [data-popup=dimmed]
+    **/
+
+    SHILLADFS.MD.popup = function(){
+
+        var module = {
+            SELF : $('[data-ui=popup]'),
+            NAME : 'UIPopupComponent',
+            DATA : {
+                ANCHOR : '[data-ui=popup]',
+                CLOSE : '[data-popup=close]',
+                POPUP : '[data-popup=cont]',
+                DIMMED : '[data-popup=dimmed]',
+            },
+            STORAGE : {},
+            STORAGEName : 'UIStorageIndex'
+        }
+
+        function init(){
+            module.SELF.length && module.SELF.each(function( index ){
+                module.STORAGE[index] = new initSetting( this, index );
+                
+                for (var key in module.STORAGE[index].elem) {
+                    $(module.STORAGE[index].elem[key]).data( module.STORAGEName, index);
+                }
+            })
+
+            bindEvents();
+        }
+
+        function initSetting( el, index ){
+            this.self = $(el);
+            this.options = $.extend({
+                index : index,
+                elem : {
+                    root: this.self,
+                    anchor : this.self,
+                    close : $(this.self.attr('href')).find( module.DATA.CLOSE ),
+                    popup : $(this.self.attr('href')),
+                    dimmed : $(this.self.attr('href')).find( module.DATA.DIMMED )[0] || true,
+                },
+                allClose : true,
+                popupDimmed : true,
+                lockClass : 'lockBody'
+            }, this.self.data());
+
+            return this.options;
+        }
+
+        function bindEvents(){
+
+            SHILLADFS.UI.elem.$doc.on(SHILLADFS.UI.events.click, module.DATA.ANCHOR, popupToggle( true ));
+            SHILLADFS.UI.elem.$doc.on(SHILLADFS.UI.events.click, module.DATA.CLOSE, popupToggle( false ));
+            SHILLADFS.UI.elem.$doc.on(SHILLADFS.UI.events.click, module.DATA.DIMMED, popupToggle( false ));
+        }
+
+        function popupDimmed( obj ) {
+
+            if (obj.elem.dimmed === true && obj.popupDimmed) {
+
+                var dim = $('<span />').css({
+                    'display' : 'block',
+                    'position' : 'fixed',
+                    'top' : 0,
+                    'left' : 0,
+                    'width' : '100%',
+                    'height' : '100%',
+                    'z-index' : 1,
+                    'background' : '#000',
+                    'opacity' : '.8'
+                    })
+                    .attr('data-popup', 'dimmed')
+                    .data( module.STORAGEName, obj.index );
+
+                    obj.elem.dimmed = dim;
+
+                obj.elem.popup.prepend( dim );
+            }
+
+        }
+
+        function popupToggle( flag ) {
+
+            return function( event ){
+
+                var obj = module.STORAGE[$(this).data( module.STORAGEName )];
+
+                obj.allClose && SHILLADFS.UI.elem.$doc.find( module.DATA.POPUP ).hide();
+                !obj.allClose && obj.elem.popup.fadeOut('fast');
+                flag && (obj.elem.popup.fadeIn('fast'), popupDimmed( obj ), SHILLADFS.UI.elem.$body.addClass( obj.lockClass ));
+                !flag && SHILLADFS.UI.elem.$body.removeClass( obj.lockClass );
+
+                event.preventDefault();
+            }
+
+        }
+
+        init();
+    }
+
+    function commonInit(){
+        SHILLADFS.MD.popup();
+    }
+
+    SHILLADFS.UI.elem.$doc.ready( commonInit );
+    
+})(this, jQuery);
---
+++ test/y3/js/ui.member.js
@@ -0,0 +1,365 @@
+/* 전역 네임스페이스 */
+var SHILLADFS = SHILLADFS || {};
+/* Membership 네임스페이스 정의 */
+SHILLADFS.Membership = {};
+
+(function(_Membership){
+
+    /* Tab Switch */
+    _Membership.TabSwitch = (function(){
+        var module = $('[data-js=tabSwitch]');
+        var moduleName = 'TabSwitch';
+
+        function init(){
+            module.length && module.each(function(){
+                $(this).data('component', new initTabArea(this));
+                console.log($(this).data());
+            })
+        }
+
+        function initTabArea(el){
+            this._this = $(el);
+            this._elements = {
+                root: this._this,
+                tabmenu : this._this.find('[data-tab-type=tab]'),
+                tabcontent : this._this.find('[data-tab-type=tab]')[0].dataset.tabTarget,
+            };
+
+            bindEvents(this._elements);
+        }
+
+        function bindEvents(el){
+
+            var tabBtn = $('a', el.tabmenu);
+            var tabList = $('li', el.tabmenu);
+            var tabContents = $(el.tabcontent, el.root);
+
+            tabBtn.on('click' + '.' + moduleName, function(e){
+                e.preventDefault();
+
+                var a = $(this);
+                var idx = a.parent('li').index();
+
+                tabList.removeClass('on');
+                a.closest('li').addClass('on');
+
+                tabContents.removeClass('active');
+                tabContents.eq(idx).addClass('active');
+            });
+
+        }
+
+        return {
+            init: init
+        }
+
+    })();
+
+    /* Accordion */
+    _Membership.Accordion = (function(){
+        var module = $('[data-js=accordion]');
+        var moduleName = 'Accordion';
+
+        function init(){
+            module.length && module.each(function(){
+                $(this).data('component', new initAccordion(this));
+                console.log($(this).data());
+            })
+        }
+
+        function initAccordion(el){
+            this._this = $(el);
+            this._elements = {
+                root : this._this,
+                anchor : this._this.find('[data-accordion-type]'),
+                chkAnchor : this._this.find('[data-accordion-type=checkbox], [data-accordion-type=radio]'),
+                btnAnchor : this._this.find('[data-accordion-type=button]'),
+                panel : this._this.find('[data-accordion-target=panel]'),
+                accordionList : this._this[0].dataset.accordionList,
+                options : {
+                    activeClass : 'active',
+                    collapsible : this._this.data('accordionCollapsible'),
+                    slide : this._this.data('accordionSlide'),
+                    toggle : this._this.find('[data-accordion-type]')[0].dataset.accordionType === 'radio' ? false : true,
+                    eventType : []
+                }
+            };
+
+            bindEvents(this._elements);
+        }
+
+        function slideUpDown(el, self){
+            var anchor = $(self);
+            var accordionList = $(el.accordionList);
+            var idx = anchor.closest(el.accordionList).index();
+            var panels = accordionList.length && el.panel;
+            var panel = accordionList.length ? el.panel.eq(idx) : el.panel;
+
+            if(el.chkAnchor.hasClass('open') && anchor.attr('data-checked') == 'false') return false;
+
+            if(el.options.slide){
+                if(el.options.toggle){
+                    el.chkAnchor.length && el.chkAnchor.toggleClass('open');
+                    el.btnAnchor.length && el.btnAnchor.toggleClass('on');
+
+                    panel.toggleClass('open');
+                    panel.stop(true).slideToggle();    
+                }else{
+                    el.options.collapsible && panels.stop(true).slideUp();
+                    panel.stop(true).slideDown();
+                }
+            }else{
+                if(el.options.toggle){
+                    el.chkAnchor.length && el.chkAnchor.toggleClass('open');
+                    el.btnAnchor.length && el.btnAnchor.toggleClass('on');
+
+                    panel.toggleClass('open');
+                    panel.toggle();
+                }else{
+                    el.options.collapsible && panels.css('display','none');
+                    panel.css('display','block');
+                }
+            }
+        }
+
+        function bindEvents(el){
+            var anchor = el.chkAnchor.find('label');
+            var button = el.btnAnchor;
+
+            button.length && button.on('click' + '.' + moduleName, function(e){
+                e.preventDefault();
+                slideUpDown(el, this);
+            });
+
+            anchor.length && anchor.on('change' + '.' + moduleName, function(e){
+                e.preventDefault();
+                slideUpDown(el, this);
+            })    
+        }
+
+        return {
+            init: init
+        }
+
+    })();
+    
+    /* Checkbox */
+    _Membership.Checkbox = (function(){
+        var module = $('[data-js=checkbox]');
+        var moduleName = 'Checkbox';
+
+        function init(){
+            module.length && module.each(function(){
+                $(this).data('component', new initCheckbox(this));
+                console.log($(this).data());
+            })
+        } 
+        
+        function initCheckbox(el){
+            this._this = $(el);
+            this._elements = {
+                root : this._this,
+                checkedAll : this._this.find('[data-checkbox-type]'),
+                checkedItem : this._this.find('[data-checkbox-target]'),
+                options : {
+                    total : this._this.find('[data-checkbox-target]').length
+                }
+            }
+
+            bindEvents(this._elements);
+        }
+
+        function bindEvents(el){
+            var checkedAll = el.checkedAll;
+            var checkedItem = el.checkedItem;
+            var total = el.options.total;
+            var checkedNum = 0;
+
+            checkedAll.on('change' + '.' + moduleName, function(){
+                var $input = $(this);
+                var isChecked = $input.is(':checked');
+
+                if(isChecked){
+                    checkedItem.prop('checked', true);
+                    setTimeout(function(){$input.parent('label').attr('data-checked', true)}, 300);
+                }else{
+                    checkedItem.prop('checked', false);
+                    setTimeout(function(){$input.parent('label').attr('data-checked', false)}, 300);
+                }
+
+                checkedNum = getCheckedNum(el);
+            });
+
+            checkedItem.on('change' + '.' + moduleName, function(){
+                checkedNum = getCheckedNum(el);
+
+                if(checkedNum === total){
+                    checkedAll.prop('checked', true);
+                    setTimeout(function(){checkedAll.parent('label').attr('data-checked', true)}, 300);
+                }else{
+                    checkedAll.prop('checked', false);
+                    setTimeout(function(){checkedAll.parent('label').attr('data-checked', false)}, 300);
+                }
+            })
+        }
+
+        function getCheckedNum(el){
+            return el.root.find('[data-checkbox-target]:checked').length;
+        }
+
+        return {
+            init: init
+        }
+
+    })();
+
+    /* Tooltip */
+    _Membership.Tooltip = (function(){
+        var module = $('[data-js=tooltip]');
+        var moduleName = 'Tooltip';
+
+        function init(){
+            module.length && module.each(function(){
+                $(this).data('component', new initTooltip(this));
+                console.log($(this).data());
+            })
+        }
+
+        function initTooltip(el){
+            this._this = $(el);
+            this._elements = {
+                root: this._this,
+                button : this._this.find('[data-tooltip-type=button]'),
+                tooltip : this._this.find('[data-tooltip-type=button]')[0].dataset.tooltipTarget,
+                arrow : this._this.find('[data-tooltip-type=button]')[0].dataset.tooltipArrow
+            };
+
+            bindEvents(this._elements);
+        }
+
+        function computedStyle(el){
+            var tooltip_width = el.root.find(el.tooltip).outerWidth();
+            var maxWidth = parseInt($(window)[0].innerWidth - 40);
+            var offsetX = el.button.offset().left;
+            var xpos = 0;
+
+            if(tooltip_width >= maxWidth){
+                tooltip_width = maxWidth;
+                xpos = -offsetX;
+            }else{
+                var tempX = (tooltip_width + offsetX) > maxWidth ? maxWidth - (tooltip_width + offsetX): -40;
+                xpos = tempX;
+            }
+
+            return {
+                'width' : tooltip_width + 'px',
+                'left' : xpos + 'px',
+                'top' : 'initial',
+                'bottom' : '25px',
+            }
+        }
+
+        function bindEvents(el){
+            var button = el.button;
+            var tooltip = el.root.find(el.tooltip);
+
+            button.on('click' + '.' + moduleName, function(e){
+                e.preventDefault();
+
+                var cssStyle = computedStyle(el);
+
+                if($(this).hasClass('active')){
+                    button.removeClass('closed');
+                }
+
+                button.addClass('active');
+                button.append(tooltip);
+
+                console.log('style', cssStyle);
+                tooltip.css(cssStyle)
+            });
+
+            $(document).on('click', '.tip_close', function(e){
+                e.preventDefault();
+                $(this).closest('[data-tooltip-type=button]').addClass('closed');
+            });
+        }
+
+        return {
+            init: init
+        }
+    })();
+
+    /* etc */
+    _Membership.Util = (function(){
+        var selectBox = $('[data-js=selectbox]');
+        var checkedButton = $('[data-js=checkedButton]');
+
+        function init(){
+
+            /* Selectbox */
+            selectBox.length && selectBox.each(function(){
+                $(this).data('component', new initSelectBox(this));
+                console.log($(this).data());
+            });
+
+            /* 인증버튼 */
+            checkedButton.length && checkedButton.each(function(){
+                $(this).data('component', new initCheckedButton(this));
+                console.log($(this).data());
+            });
+        }
+
+        /* Selectbox - 값 선택시 스타일 변경 */
+        function initSelectBox(el){
+            this._this = $(el);
+            this._selectbox = this._this.find('.form_sel select');
+
+            this._selectbox.on('change', function(){
+                var value = $('option:selected', $(this)).val();
+                var index = $('option', $(this)).index($('option:selected', $(this)));
+
+                index === 0 ? $(this).parent('.form_sel').addClass('basic') : $(this).parent('.form_sel').removeClass('basic');
+                if(value === 'direct'){
+                    var $add_field = $(el).find('.form_inp.add_field').show();
+                    $add_field.find('input[type=text]').focus();
+                }else{
+                    $(el).find('.form_inp.add_field').hide();
+                }
+            });
+        }
+
+        /* 인증버튼 - 선택시 활성 */
+        function initCheckedButton(el){
+            this._this = $(el);
+            this._button = this._this.find('a');
+
+            this._button.on('click', function(e){
+                e.preventDefault();
+
+                $(el).find('li').removeClass('on').addClass('off');
+                $(el).find('li').attr('data-checked', false);
+
+                $(this).parent('li').removeClass('off').addClass('on');
+                $(this).parent('li').attr('data-checked', true);
+            });
+        }
+
+        return {
+            init: init
+        }
+
+    })();
+
+
+})(SHILLADFS.Membership);
+
+$(document).on('ready', function(e){
+    SHILLADFS.Membership.TabSwitch.init();
+    SHILLADFS.Membership.Accordion.init();
+    SHILLADFS.Membership.Checkbox.init();
+    SHILLADFS.Membership.Util.init();
+    SHILLADFS.Membership.Tooltip.init();
+
+    console.log($(window));
+});
