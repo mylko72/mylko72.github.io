a4bc656e7d0ee16bab8d12431221932d0823be4b
Kwanoh Lee <mypmk@naver.com>
2023-01-17 15:33:00 +0900

    패럴렉스 스크롤 애니메이션 추가

---
+++ parallax_scroll/js/main.js
@@ -0,0 +1,185 @@
+const ui = {};
+
+((_ui) => {
+    const sceneInfo = [
+        {
+            // 0
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+            },
+            values: {
+                messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+                messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+                messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
+            }
+        },
+        {
+            // 1
+            type: 'sticky',
+            heightNum: 5, // type normal에서는 필요 없음
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1'),
+            }
+        },
+        {
+            // 2
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2'),
+            },
+        },
+        {
+            // 3
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3'),
+            }
+        }
+    ];
+
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된 Scene(scroll-section)
+    let enterNewScene = false; // 새로운 scene이 시작된 순간 true
+
+    function setLayout() {
+        // 각 스크롤 섹션의 높이 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            if (sceneInfo[i].type === 'sticky') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            } else if (sceneInfo[i].type === 'normal') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].objs.content.offsetHeight + window.innerHeight * 0.5;
+            }
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+        yOffset = window.pageYOffset;
+
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            //totalScrollHeight가 현재 스크롤 위치(window.pageYOffset)보다 클때가 현재 활성화된 스크롤 섹션
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);        
+    }
+
+    function scrollLoop() {
+        enterNewScene = false;
+        prevScrollHeight = 0;	
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        // console.log('prevScrollHeight', prevScrollHeight);
+
+        // 현재 스크롤 위치가 이전 스크롤 높이와 현재 스크롤 섹션의 높이를 더한 값보다 클 때 currentScene은  증가
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {   
+            enterNewScene = true;
+            currentScene++
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+    
+        // 스크롤 위치가 이전 스크롤 높이보다 작아지면 currentScene은 감소
+        if (yOffset < prevScrollHeight) {
+            enterNewScene = true;
+           // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+           if (currentScene === 0) return;
+           currentScene--;
+           document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }  
+
+        console.log(`현재 스크롤섹션은 ${currentScene}`)
+
+        if (enterNewScene) return;
+
+        playAnimation();
+    }
+
+    function calcValues(values, currentYOffset){
+        let rv;
+
+        //현재 스크롤섹션에서 스크롤된 범위를 비율로 구하기
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        // values의 타이밍속성이 3번째 있는지 분기처리
+        if (values.length === 3) {
+            // start ~ end 사이에 애니메이션 실행
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart;
+
+            if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+            } else if (currentYOffset < partScrollStart) {
+                rv = values[0];
+            } else if (currentYOffset > partScrollEnd) {
+                rv = values[1];
+            }
+        } else {
+            // scrollRatio는 0에서 1까지이며 전달된 values의 값에 따라 비율에 따른 범위를 구하는 공식
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
+    
+        return rv
+    }
+
+    function playAnimation() {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+    
+        switch (currentScene) {
+            case 0:
+                // console.log('0 play');
+                if (scrollRatio <= 0.22) {
+                    // in
+                    objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
+                    objs.messageA.style.transform = `translate3d(0, ${calcValues(values. messageA_translateY_in, currentYOffset)}%, 0)`;
+                }else{
+                    // out
+                    objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
+                    objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`;
+                }
+                
+                break;
+    
+            case 1:
+                // console.log('2 play');
+                break;
+    
+            case 2:
+                // console.log('3 play');
+                break;
+        }
+    }
+
+    window.addEventListener('load', () => {
+        setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이를 세팅하도록 실행
+    })
+
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
+
+    window.addEventListener('resize', setLayout);
+
+    setLayout();
+
+  })(ui);
